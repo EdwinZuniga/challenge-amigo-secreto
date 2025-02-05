27135ee219420e1f10f4097046c2fac09c8f1133
EdwinZuniga <edwinzuniga@nehemiahiu.com>
2025-02-04 23:32:47 -0600

    Función de agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 let listaAmigos = [];
 let amigosSorteados = [];
+
+function agregarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nombre = inputAmigo.value.trim();
+    
+    // Validación de campo vacío
+    if (nombre === '') {
+        alert('Porfavor ingrese un nombre valido');
+        return;
+    }
+    
+    // Agrega el nombre a la lista
+    listaAmigos.push(nombre);
+    
+    // Limpia el input
+    inputAmigo.value = '';
+    
+    // Actualizar la lista visual
+    actualizarListaAmigos();
+}
